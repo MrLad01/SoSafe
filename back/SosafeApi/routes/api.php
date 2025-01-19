21a6aba58e94fe320381de08f1f0de5e62231431
AyobamiLad <moriahberry2222@gmail.com>
2025-01-19 10:01:34 +0100

    update for login

--- back/SosafeApi/routes/api.php
+++ back/SosafeApi/routes/api.php
@@ -54,9 +54,9 @@ Route::post('/get',[BiodataController::class, 'record']);
 Route::get('/biodata2/form/{formNo}', [Biodata2Controller::class, 'findByFormNo']);
 Route::get('/biodata2/form/phone/{phoneNo}', [Biodata2Controller::class, 'findByPhoneNo']);
 
-Route::get('/biodata2', [Biodata2Controller::class, 'index']);
-Route::get('/biodata2/{id}', [Biodata2Controller::class, 'show']);
-Route::get('/biodata2/form/{formNo}', [Biodata2Controller::class, 'findByFormNo']);
+// Route::get('/biodata2', [Biodata2Controller::class, 'index']);
+// Route::get('/biodata2/{id}', [Biodata2Controller::class, 'show']);
+// Route::get('/biodata2/form/{formNo}', [Biodata2Controller::class, 'findByFormNo']);
 Route::middleware([JwtMiddleware::class,'role:admin'])->group(function () {
     
     Route::post('user/register', [UserController::class, 'createAdmin']);
