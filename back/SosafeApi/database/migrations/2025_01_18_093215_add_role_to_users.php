81ed918b1a6f487f5d047b1dbc93ade4e5443417
siadev1 <siason2004@gmail.com>
2025-04-28 14:18:46 +0100

    auth role update

--- back/SosafeApi/database/migrations/2025_01_18_093215_add_role_to_users.php
+++ back/SosafeApi/database/migrations/2025_01_18_093215_add_role_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->nullable()->after('Area');
+            $table->string('role')->nullable()->after('area');
             // $table->string('')->nullable()->after('password');
 
         });
