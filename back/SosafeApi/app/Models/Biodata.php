a2c53da927f4cb8275387246c75df997dc2fdf36
MrLad01 <ladayodeji@gmail.com>
2026-06-02 04:58:45 +0100

    recreate biodatas table with new structure

--- back/SosafeApi/app/Models/Biodata.php
+++ back/SosafeApi/app/Models/Biodata.php
@@ -10,35 +10,47 @@ class Biodata extends Model
     use HasFactory;
 
     protected $fillable = [
-        'code',
-        'form_no',
-        'firstname',
-        'lastname',
-        'othername',
-        'address',
-        'phone_no',
-        'dob',
-        'sex',
-        'community',
-        'za_command',
-        'division_command',
-        'service_code',
-        'position',
-        'date_engage',
-        'rank',
-        'nok',
-        'relationship',
-        'nok_phone',
-        'photo',
-        'qualification',
+        'SNO',
+        'FNO',
+        'SNAME',
+        'FNAME',
+        'ONAME',
+        'ADDRESS',
+        'PHONE',
+        'NIN',
+        'DOB',
+        'SEX',
+        'CITY',
+        'ZONE',
+        'AREA',
+        'SERVNO',
+        'POSITION',
+        'ENLISTED',
+        'RANK',
+        'NOK',
+        'RELATION',
+        'NOKNO',
+        'CAPTURED',
+        'QUALIFICATION',
     ];
 
-    // Optional: If you want to control timestamps
-    // public $timestamps = true;
-
-    // Cast dates properly
     protected $casts = [
-        'dob'         => 'date',
-        'date_engage' => 'date',
+        'DOB'      => 'date',
+        'ENLISTED' => 'date',
     ];
+
+    public function zone()
+    {
+        return $this->belongsTo(Zone::class, 'ZONE');
+    }
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class, 'AREA');
+    }
+
+    public function division()
+    {
+        return $this->belongsTo(Division::class, 'CITY');
+    }
 }
